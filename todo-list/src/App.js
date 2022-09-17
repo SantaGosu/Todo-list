1c348f6da425219fdd228d019d8000bcc634682f
SantaGosu <taz.c.munoz@gmail.com>
2022-09-17 15:16:20 +0000

    created react app

---
+++ todo-list/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input placeholder='What needs to be done?'/>
+      <button ></button>
+    </div>
+  );
+}
+
+export default App;
