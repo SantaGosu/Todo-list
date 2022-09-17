6e0c34fbce6300f19079c37d5accead7ba8ff010
SantaGosu <taz.c.munoz@gmail.com>
2022-09-17 15:29:05 +0000

    imported and temporarily put in useState

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,11 +1,13 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+const [id, setId] = useState('')
+
   return (
     <div className="App">
-      <input placeholder='What needs to be done?'/>
-      <button ></button>
+      <input type="text" id='' placeholder='What needs to be done?' onChange={ e => setId(e.target.value)}/>
+      <button onClick={setId}></button>
     </div>
   );
 }
