ff3029be96cbfae15fdc8674c5f3982b881b7447
SantaGosu <taz.c.munoz@gmail.com>
2022-09-17 20:47:06 +0000

    fixed

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,26 +2,12 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [items, setItems] = useState([]);
-
-  const addItem = () => {
-    setItems([...items, {
-      id: items.length,
-      value: Math.floor(Math.random()*10 + 1)
-    }])
-  }
+const [id, setId] = useState('')
 
   return (
     <div className="App">
-      <input type="text" placeholder='What needs to be done?' onChange={ e => setItems(e.target.value)}/>
-      <ul>
-        { items.map(item => item (
-          <li key={item.id}>{item.value}</li>
-        ))
-
-        }
-      </ul>
-      <button onClick={addItem}></button>
+      <input type="text" id='' placeholder='What needs to be done?' onChange={ e => setId(e.target.value)}/>
+      <button onClick={setId}></button>
     </div>
   );
 }
