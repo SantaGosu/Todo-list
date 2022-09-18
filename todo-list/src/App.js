de43c3c39f86facac8845921bc8dd6fd0576c52b
SantaGosu <taz.c.munoz@gmail.com>
2022-09-18 15:40:41 +0000

    webpage is giving ul its own padding left and i cant remove it

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-const [item, setItem] = useState('');
+const [item, setItem] = useState('Take a walk');
 const [list, setList] = useState([]);
 
 useEffect(() => {
   setList([...list, item])
 }, [item]);
 
-const preventRefresh = (event) => {
+const preventRefresh = event => {
   event.preventDefault();
+  console.log("submitted")
+  
 }
 
 
   return (
-    <div className="App">
-      <form >
-      <input type="text" placeholder='What needs to be done?' onSubmit={ (e) => setItem(e.target.value)} />
+    <div className="container">
+      <form onSubmit={preventRefresh} >
+        <h1>Todo list</h1>
+      <input size="20" id="textBox" type="text"  placeholder='What needs to be done?' onDoubleClick={(e) => setItem(e.target.value)} />
+        <ol>{list.map( (value, index) => 
+          <li key={index}>{value}</li>)}
+        </ol>
       </form>
-      <ul>{list.map( (value, index) => 
-        <li key={index}>{value}</li>)}
-      </ul>
     </div>
   );
 }
