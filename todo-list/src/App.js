ae5ae09d47a91cadd70e0f1bf4e09aadad9303d7
SantaGosu <taz.c.munoz@gmail.com>
2022-09-19 17:52:12 +0000

    made a function to remove item from state, just need to impliment .slice or pop

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -13,16 +13,21 @@ useEffect(() => {
 
 const preventRefresh = event => {
   event.preventDefault();
-  
 }
 
+const removeItem = () => {
+  console.log("remove " + item)
+}
+  
   return (
     <div className="container">
       <form onSubmit={preventRefresh} >
         <h1>Todos</h1>
       <Searchbar setItem={setItem} />
       <ul className='list-group'>{list.map( (value, index) => 
-        <li className='list-group-item' key={index}>{value}</li>)}
+        <li className='list-group-item' key={index}>{value}
+        <button type='button' className="btn btn-default"onClick={(e) => removeItem(item)}>x</button>
+        </li>)}
         <li className='list-group-item'> todos left</li>
       </ul>
       </form>
