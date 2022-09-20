32830605246ec45c55d75246cd123e1c408689d2
SantaGosu <taz.c.munoz@gmail.com>
2022-09-20 00:57:10 +0000

    added the button that delets the li from ul

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -4,34 +4,33 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Searchbar from './components/Searchbar/Searchbar';
 
 function App() {
-const [item, setItem] = useState('Take a walk');
-const [list, setList] = useState([]);
-const [isShown, setIsShown] = useState(true);
-
-const handleClick = event => {
-  setIsShown(current => !current);
-};
+  const [item, setItem] = useState('Take a walk');
+  const [list, setList] = useState([]);
 
 
-useEffect(() => {
-  setList([...list, item])
-}, [item]);
+  const deleteTask = (index) => {
+    setList(list.filter((element, i) => i != index))
+  }
 
-const preventRefresh = event => {
-  event.preventDefault();
-}
+  useEffect(() => {
+    setList([...list, item])
+  }, [item]);
+
+  const preventRefresh = event => {
+    event.preventDefault();
+  }
 
   return (
     <div className="container">
       <form onSubmit={preventRefresh} >
         <h1>Todos</h1>
-      <Searchbar setItem={setItem} />
-      <ul className='list-group'>{list.map( (value, index) => 
-        <li className='list-group-item' key={index} style={{display: isShown ? 'block' : 'none'}}>{value}
-        <button onClick={handleClick} type='button' className="btn btn-default">x</button>
-        </li>)}
-        <li className='list-group-item'> todos left</li>
-      </ul>
+        <Searchbar setItem={setItem} />
+        <ul className='list-group'>{list.map((value, index) =>
+          <li className='list-group-item' key={index}>{value}
+            <button type='button' onClick={()=>deleteTask(index)} className="btn btn-default">x</button>
+          </li>)}
+          <li className='list-group-item'> todos left</li>
+        </ul>
       </form>
     </div>
   );
