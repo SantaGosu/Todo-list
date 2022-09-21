e7de0bebac18eee273b82a32e0928b54852ac6fa
SantaGosu <taz.c.munoz@gmail.com>
2022-09-21 01:32:32 +0000

    implimented GET and PUT fetch functions into code

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -7,14 +7,47 @@ function App() {
   const [item, setItem] = useState('Take a walk');
   const [list, setList] = useState([]);
 
+ const funcGET = () => {
+      fetch("http://assets.breatheco.de/apis/fake/todos/user/SantaG',%7B", {
+        method: "GET",
+        headers: {"Content-type": "application/json;charset=UTF-8"}
+      })
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(err => console.log("ERROR FOR GET: " + err.message))
+    }
 
-  const deleteTask = (index) => {
-    setList(list.filter((element, i) => i != index))
-  }
+  let data = 
+      [
+        {
+          "label": "sample task 2",
+          "done": false
+        },
+        {
+          "label": "THIS IS A TEST",
+          "done": false}
+      ];
+    const getPUT = () => {
+      fetch("http://assets.breatheco.de/apis/fake/todos/user/SantaG',%7B", {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {"Content-type": "application/json;charset=UTF-8"}
+      })
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(err => console.log("ERROR to get PUT: " + err.message))
+    }
+  
+    
 
   useEffect(() => {
+    funcGET()
     setList([...list, item])
   }, [item]);
+
+  const deleteTask = (index) => {
+    setList(list.filter((element, i) => i !== index))
+  }
 
   const preventRefresh = event => {
     event.preventDefault();
@@ -29,7 +62,7 @@ function App() {
           <li className='list-group-item' key={index}>{value}
             <button type='button' onClick={()=>deleteTask(index)} className="btn btn-default">x</button>
           </li>)}
-          <li className='list-group-item'> todos left</li>
+          <li className='list-group-item'> to dos left</li>
         </ul>
       </form>
     </div>
