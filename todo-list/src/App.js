597397166bf026c9acef8e58464dfcdb3f6e3dd6
SantaGosu <taz.c.munoz@gmail.com>
2022-10-18 18:59:03 +0000

    finished todolist

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -4,64 +4,70 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Searchbar from './components/Searchbar/Searchbar';
 
 function App() {
-  const [item, setItem] = useState('Take a walk');
+ 
   const [list, setList] = useState([]);
 
-  console.log(item)
-
-  //should get todo from the api, not sure why its not, figure out after class
   const GetFunction = () => {
     fetch("https://assets.breatheco.de/apis/fake/todos/user/SantaG',%7B", {
-      method: 'GET',
-      headers: {
-        "Content-Type": "application/json"
-    }
-  })
-     .then((res) => res.json())
-     .then((data) => (data))
+      method: "GET",
+      headers: {"Content-type": "application/json;charset=UTF-8"}
+    })
+    .then(response => response.json())
+    .then(data => setList(data))
+    .catch(err => console.log("ERROR FOR GET: " + err.message))
   }
 
-  const PutFunction = () => {
+ 
+
+  const PutFunction = (todo) => {
+    let newList = list.concat({"label":todo, "done": false})
+    setList(newList);
+
+
     fetch("https://assets.breatheco.de/apis/fake/todos/user/SantaG',%7B", {
       method: "PUT",
-      body: JSON.stringify(
-        //THIS IS WHERE THE UPDATED TODO LIST SHOULD GO
-      ),
-      headers: {
-        "Content-Type": "application/json"
-      }
-  })
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .then((data) => setList(data))
-  .catch(err => console.log("ERROR to get PUT: " + err.message))
+      body: JSON.stringify(newList),
+      headers: {"Content-type": "application/json;charset=UTF-8"}
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log("ERROR to get PUT: " + err.message))
   }
 
+
   useEffect(() => {
-    PutFunction();
-    setList([...list, item])
-  }, [item]);
+    GetFunction();
+  }, []);
 
 
   const deleteTask = (index) => {
-    setList(list.filter((element, i) => i !== index))
+    let newList = list.filter((element, i) => i !== index);
+    setList(newList);
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/SantaG',%7B", {
+      method: "PUT",
+      body: JSON.stringify(newList),
+      headers: {"Content-type": "application/json;charset=UTF-8"}
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log("ERROR to get PUT: " + err.message))
   }
 
   const preventRefresh = event => {
     event.preventDefault();
   }
 
-  console.log( "this is the list" + list)
+
   return (
     <div className="container">
-      <form onSubmit={preventRefresh} >
+      <form onSubmit={preventRefresh}>
         <h1>Todos</h1>
-        <Searchbar setItem={setItem} />
+        <Searchbar PutFunction={PutFunction} />
         <ul className='list-group'>{list.map((value, index) =>
-          <li className='list-group-item' key={index}>{value}
+          <li className='list-group-item' key={index}>{value.label}
             <button type='button' onClick={()=>deleteTask(index)} className="btn btn-default">x</button>
           </li>)}
-          <li className='list-group-item'>{list.length} to dos left</li>
+          <li className='list-group-item'>{list.length} todo's left</li>
         </ul>
       </form>
     </div>
--- todo-list/src/components/Searchbar/Searchbar.jsx
+++ todo-list/src/components/Searchbar/Searchbar.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { useState } from "react";
 
 const Searchbar = (props) => {
-    const [id, setId] = useState('');
+    const [inputValue, setInputValue] = useState('');
 
     const onClick = () => {
-        props.setItem(id)
+        props.PutFunction(inputValue)
     }
 
     return (
         <>
-            <input size="61" id="textBox" type="text" placeholder='What needs to be done?' onChange={(e) => setId(e.target.value)} />
+            <input size="61" id="textBox" type="text" placeholder='What needs to be done?' onChange={(e) => setInputValue(e.target.value)} />
             <input id="search-input" className="btn btn-primary mb-2" type="submit" value="Search" onClick={onClick}/>    
         </>
     )
